c96520ad4bcb67cf2c984f8503aa8b5d832a8549
Munggill <wz2635@naver.com>
2023-07-10 14:24:20 +0900

    다시접근

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wetube Reloaded
+
+GlobalRouter
+/ => HOME
+/join => Join
+/login => Login
+/search => Search
+
+/users/edit => Edit user
+/users/delete => Delete user
+
+/videos/watch => Watch Video
+/videos/edit => Edit Video
+/videos/delete => Delete Video
+/videos/comments => Comment on a video
--- src/server.js
+++ src/server.js
@@ -1,38 +1,17 @@
 import express from "express";
- 
-const PORT = 3999;
+import logger from "morgan";
+import globalRouter from "./router/globalRouter";
+import userRouter from "./router/userRouter";
+import videoRouter from "./router/videoRouter";
 
+const PORT = 3999; 
 const app = express();
+app.use(logger("dev"));   
 
-/**
- 서버에서 클라이언트로 반환하는 것은 어떤 형태든 가능하다.
- 이하 등등
- 1. Json 형태
- 2. 문자열 형태
- 3. HTML형태
- 4. Status Code
- */
 
-function logger (req, res, next) {
-    console.log(`${req.method} ${req.url}`);
-    //return res.send({ message: "MiddleWareTest ==> Last res.end(json)111"} ); 
-    next();
-}
 
-const getHandelHome = (req, res) => {
-    return res.send({ message: "MiddleWareTest ==> Last res.end(json)222"} ); 
-}
-
-const getLoginHandle = (req, res) =>{
-    return res.send("<h1> Login Handle</h1>"); 
-}
-const getHandleProtected = (req, res) =>{
-    return res.send("Welcome to the private lounge.");
-}
-
-app.use(logger);  
-
-app.get("/", getHandelHome);
-app.get("/login",getLoginHandle);
-app.get("/protected",getHandleProtected);
-app.listen(PORT, () => console.log(`Server listenting on http://localhost:${PORT}`));    
+app.use("/", globalRouter);
+app.use("/users", userRouter); 
+app.use("/videos", videoRouter); 
+ 
+app.listen(PORT, () => console.log(`Server listenting on http://localhost:${PORT}`));
