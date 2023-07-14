0e9de29e2a00bacda017dc7b99dffc0a27b9d32a
Munggill <wz2635@naver.com>
2023-07-14 18:25:13 +0900

    Pug Block에 객체배열 전달

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,14 +1,39 @@
-const fakeUser ={
-    username:"안명길",
-    loggedIn: false,
-}
 
 // =======================================================
 // server.js에서 pug 관련 설정을 해주었기 때문에 Express에서 
 // pug 모듈로 render를 해준다.
 // render는 view탬플릿과 탬플릿으로 보낼 변수의 인자를 같는다.
 // =======================================================
-export const trending = (req, res) => res.render("home", {pageTitle : "Home", fakeUser:fakeUser});
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title : "First Video",
+            rating : 5,
+            comments : 2,
+            createdAt : "2 minutes ago",
+            views : 59,
+            id : 1
+        },     
+        {
+            title : "Second Video",
+            rating : 5,
+            comments : 2,
+            createdAt : "2 minutes ago",
+            views : 59,
+            id : 1
+        },        
+        {
+            title : "Third Video",
+            rating : 5,
+            comments : 2,
+            createdAt : "2 minutes ago",
+            views : 59,
+            id : 1
+        },        
+    ];
+    return res.render("home", {pageTitle : "Home", videos });
+} 
+    
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search =(req, res) => res.send("Search");
