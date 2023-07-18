3f9642feedb6b0dae5b19268aa274b547f7ad7cb
Munggill <wz2635@naver.com>
2023-07-18 23:09:55 +0900

    video 탬플릿에서 파라매터를 받아 상세페이지 화면을 이동할 수 있도록 수정, 상세페이지 에서는 화면 수정 탬플릿으로 이동할 수 있도록 기능 추가

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,44 +1,49 @@
+let videos = [
+    {
+        title : "First Video",
+        rating : 5,
+        comments : 2,
+        createdAt : "2 minutes ago",
+        views : 1,
+        id : 1
+    },     
+    {
+        title : "Second Video",
+        rating : 5,
+        comments : 2,
+        createdAt : "2 minutes ago",
+        views : 59,
+        id : 2
+    },        
+    {
+        title : "Third Video",
+        rating : 5,
+        comments : 2,
+        createdAt : "2 minutes ago",
+        views : 59,
+        id : 3
+    },        
+];
 
 // =======================================================
 // server.js에서 pug 관련 설정을 해주었기 때문에 Express에서 
 // pug 모듈로 render를 해준다.
 // render는 view탬플릿과 탬플릿으로 보낼 변수의 인자를 같는다.
 // =======================================================
-export const trending = (req, res) => {
-    const videos = [
-        {
-            title : "First Video",
-            rating : 5,
-            comments : 2,
-            createdAt : "2 minutes ago",
-            views : 59,
-            id : 1
-        },     
-        {
-            title : "Second Video",
-            rating : 5,
-            comments : 2,
-            createdAt : "2 minutes ago",
-            views : 59,
-            id : 1
-        },        
-        {
-            title : "Third Video",
-            rating : 5,
-            comments : 2,
-            createdAt : "2 minutes ago",
-            views : 59,
-            id : 1
-        },        
-    ];
+export const trending = (req, res) => {    
     return res.render("home", {pageTitle : "Home", videos });
 } 
     
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-export const search =(req, res) => res.send("Search");
-export const upload =(req, res) => {
-    console.log("TEST>");
-    res.send("<h1>Upload</h1>");
+export const watch = (req, res) => {
+    const { id } = req.params; 
+    const video = videos[id-1];
+    return res.render("watch", {pageTitle : `Watching ${video.title}`, video:video });
+};
+export const edit = (req, res) => {    
+    const { id } = req.params; 
+    const video = videos[id-1];
+    return res.render("edit", {pageTitle : "test", video});
 }
+export const search =(req, res) => res.send("Search");
+export const upload =(req, res) =>  res.send("<h1>Upload</h1>");
 export const deleteVideo =(req, res) => res.send("DeleteVideo");
