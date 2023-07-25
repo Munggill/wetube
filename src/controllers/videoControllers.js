767a200c084010101522b39180a26d8374f1405f
Munggill <wz2635@naver.com>
2023-07-25 22:46:18 +0900

    가짜 데이터베이스(배열) 지우고 mongoose로 데이터베이스 통신 테스트

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,54 +1,29 @@
-let videos = [
-    {
-        title : "First Video",
-        rating : 5,
-        comments : 2,
-        createdAt : "2 minutes ago",
-        views : 1,
-        id : 1
-    },     
-    {
-        title : "Second Video",
-        rating : 5,
-        comments : 2,
-        createdAt : "2 minutes ago",
-        views : 59,
-        id : 2
-    },        
-    {
-        title : "Third Video",
-        rating : 5,
-        comments : 2,
-        createdAt : "2 minutes ago",
-        views : 59,
-        id : 3
-    },        
-];
+import Video from "../models/Video";
 
 // =======================================================
 // server.js에서 pug 관련 설정을 해주었기 때문에 Express에서 
 // pug 모듈로 render를 해준다.
 // render는 view탬플릿과 탬플릿으로 보낼 변수의 인자를 같는다.
 // =======================================================
-export const trending = (req, res) => {    
-    return res.render("home", {pageTitle : "Home", videos });
-} 
-    
+export const home = (req, res) => {    
+    Video.find({},(error, videosDocument) => {
+        console.log("errors", error);
+        console.log("Document", videosDocument);
+    });
+    return res.render("home", {pageTitle : "Home", videos:[] });
+};
 export const watch = (req, res) => { 
-    const { id } = req.params; 
-    const video = videos[id-1];
-    return res.render("watch", {pageTitle : `Watching : ${video.title}`, video:video });
+    const { id } = req.params;  
+    return res.render("watch", {pageTitle : `Watching :` });
 };
 export const getEdit = (req, res) => {    
-    const { id } = req.params; 
-    const video = videos[id-1];
-    return res.render("edit", {pageTitle : `Editing : ${video.title}`, video});
+    const { id } = req.params;  
+    return res.render("edit", {pageTitle : `Editing :`});
 };
 export const postEdit = (req, res) => {   
     const { id } = req.params;
     //Form Action의 Value는 req.body로 받는다.
-    const title = req.body.title;
-    videos[id-1].title = title;
+    const title = req.body.title; 
     //const { title } = req.body;
     console.log(title);
     
@@ -60,15 +35,7 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = (req, res) => {
-    const newVideo ={ 
-        title : req.body.title,
-        rating : 0,
-        comments : 0,
-        createdAt : "jst now",
-        views : 0,
-        id : videos.length + 1 
-    }
-    videos.push(newVideo);
+
     // here we will add a video to the videos array.
     return res.redirect("/");
 };
