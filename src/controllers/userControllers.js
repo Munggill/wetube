6519213e86a4c1dfe031cf1085a3b749d8631f80
Munggill <wz2635@naver.com>
2023-08-24 22:40:10 +0900

    로그인 화면 생성 및 BackEnd에서 유저명 전달받아, Mongoose를 사용하여 해당 유저명이 존재하는 유저명인지 확인하는 로직 생성

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -18,19 +18,41 @@ export const postJoin = async (req, res) => {
             errorMessage:"이미 사용중인 유저명/이메일 입니다.",
         });
     };    
-    
-    await UserModel.create({        
-        name : name,         
-        email,         
-        username,        
-        password,
-        location
-    });    
-    return res.redirect("/login");
-};
+    try{
+
+        await UserModel.create({        
+            name : name,         
+            email,         
+            username,        
+            password,
+            location
+        });    
+        return res.redirect("/login");
+    } catch(error) {         
+        return res.status(404).render("upload", {            
+            pageTitle:"UploadVideo", 
+            errorMessage : error._message
+        });
+    };
+}
+export const getLogin = (req, res) => res.render("Login", {
+    pageTitle:"Login"
+});
+
+export const postLogin = async (req, res) => {
+    const { username, password } = req.body;
+    const exists = await UserModel.exists({username:username});
+    if(!exists){
+        return res.status(400).render("login", {
+            pageTitle:"Login", 
+            errorMessage:`${username}의 유저명이 존재하지 않습니다.`
+        });
+    }
+    res.end();
+}
+
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("remove");
-export const login = (req, res) => res.send("Login");
+export const remove = (req, res) => res.send("remove"); 
 export const logout = (req, res) => res.send("Log out"); 
 export const see = (req, res) => res.send("see User");   
 
--- src/router/rootRouter.js
+++ src/router/rootRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getJoin, postJoin, login } from "../controllers/userControllers";
+import { getJoin, postJoin, getLogin, postLogin } from "../controllers/userControllers";
 import { home, search } from "../controllers/videoControllers";
 
 const rootRouter = express.Router();   
@@ -7,7 +7,7 @@ const rootRouter = express.Router();
 rootRouter.get("/search", search); 
 rootRouter.route("/join").get(getJoin).post(postJoin);
 rootRouter.get("/", home); 
-rootRouter.get("/login", login); 
+rootRouter.route("/login").get(getLogin).post(postLogin);
 
 export default rootRouter;
 
