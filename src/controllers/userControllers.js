0e88c78c939ff154b916f09a0053a50a5b0152e8
Munggill <wz2635@naver.com>
2023-09-11 20:37:26 +0900

    소셜 로그인(Github)로 Redirect하는 로직이 HTML탬플릿 파일에 있던것을 Controller에서 변수화 해서 처리 및 Github의 CallBack 반환되는 것 확인

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -63,9 +63,21 @@ export const postLogin = async (req, res) => {
     return res.redirect("/");
 }
 
+export const startGithubLogin = (req, res) => {    
+    const baseUrl = `https://github.com/login/oauth/authorize`;
+    const config = {
+        clientId : "61c61c540b7a9c650293",
+        allow_singup : false,
+        scope : "read:user user:email",
+    };
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("remove"); 
-export const logout = (req, res) => res.send("Log out"); 
+export const logout = (req, res) => res.send("Log out");  
 export const see = (req, res) => res.send("see User");   
 
 // 재 Commit을 위한 임시 주석
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
-import {logout, edit, remove, see} from "../controllers/userControllers"
+import {logout, edit, remove, startGithubLogin, see} from "../controllers/userControllers"
 const userRouter = express.Router(); 
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
-userRouter.get(":id", see); 
+userRouter.get("/github/start", startGithubLogin); 
+
+userRouter.get("/:id", see);
     
 export default userRouter;
 
