d79ce89621ba66311551a3c2e13cbc4034f68395
Munggill <wz2635@naver.com>
2023-09-18 22:03:52 +0900

    Fetch로 Git에서 Json 데이터를 받아옴.. 하지만 에러가 남, 내일 고쳐볼 것

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -77,15 +77,24 @@ export const startGithubLogin = (req, res) => {
     return res.redirect(finalUrl);
 };
 
-export const finishGithubLogin = (req, res) => {
+export const finishGithubLogin = async(req, res) => {   
     const baseUrl = "https://github.com/login/oauth/access_token";
     const config = {
         client_id:process.env.GH_CLIENT,
         client_secret: process.env.CH_SECRET,
         code : req.query.code
     };
-    const param = new URLSearchParams(config).toString();
+    console.log(config);
+    const params = new URLSearchParams(config).toString();
     const finalUrl = `${baseUrl}?${params}`; 
+    const data = await fetch(finalUrl, {
+        method:"POST",
+        headers:{
+            Accept : "application/json"
+        },
+    })
+    const json = await data.json();
+    console.log(json);
 };
 
 export const edit = (req, res) => res.send("Edit User");
